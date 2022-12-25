26a78c835ce1e8d5b30ac38d83e37e24fd100e7a
shimozuki <robbishimozuki@gmail.com>
2022-12-25 12:42:28 +0800

    master

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\MessageBag;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,31 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|string', //VALIDASI KOLOM USERNAME
+            //TAPI KOLOM INI BISA BERISI EMAIL ATAU USERNAME
+            'password' => 'required|string|min:8',
+        ]);
+    
+        //LAKUKAN PENGECEKAN, JIKA INPUTAN DARI USERNAME FORMATNYA ADALAH EMAIL, MAKA KITA AKAN MELAKUKAN PROSES AUTHENTICATION MENGGUNAKAN EMAIL, SELAIN ITU, AKAN MENGGUNAKAN USERNAME
+        $loginType = filter_var($request->email, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+      
+        //TAMPUNG INFORMASI LOGINNYA, DIMANA KOLOM TYPE PERTAMA BERSIFAT DINAMIS BERDASARKAN VALUE DARI PENGECEKAN DIATAS
+        $login = [
+            $loginType => $request->email,
+            'password' => $request->password,
+        ];
+      
+        //LAKUKAN LOGIN
+        if (auth()->attempt($login)) {
+            //JIKA BERHASIL, MAKA REDIRECT KE HALAMAN HOME
+            return redirect()->route('admin.home');
+        }
+        $errors = new MessageBag(['email'=>['Email/Username atau Password salah!']]);
+        //JIKA SALAH, MAKA KEMBALI KE LOGIN DAN TAMPILKAN NOTIFIKASI 
+        return redirect()->route('login')->withErrors($errors);
     }
 }
